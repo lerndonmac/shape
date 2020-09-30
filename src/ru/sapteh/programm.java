e8a89bb0735da3d11a1f8b2272dc7edbef66d388
AlexLifeless <lin26618@gmail.com>
2020-09-30 14:11:14 +0300

    dosomthing

--- src/ru/sapteh/programm.java
+++ src/ru/sapteh/programm.java
@@ -1,10 +1,4 @@
 package ru.sapteh;
-
-<<<<<<< HEAD
-public class programm {
-    public static void main(String[] args) {
-        System.out.println("hellow world");
-=======
 import java.util.Scanner;
 
 public class programm {
@@ -26,27 +20,17 @@ public class programm {
         }
     }
     public static String personIsEmpti (Person person){
-        String nE="";
-        String aE="";
-        String dE="";
-        String err;
-        if ((person.getName()+person.getDate()+person.getAge())!=null){
-
+        String err="";
             if (person.getName()==""){
-                nE="имени нету ";
+                err += "имени нету ";
             } if (person.getAge()==0){
-                aE="возроста нету ";
+                err+="возроста нету ";
             } if (person.getDate()==0){
-                dE="даты нету ";
+                err+="даты нету ";
             }
-            err = nE+aE+dE ;
-            return err;
-        }
-
-
-
+            if (err.equals("")){
+            return err;}
         return "";
->>>>>>> 0f34933... create class
     }
 
 }
